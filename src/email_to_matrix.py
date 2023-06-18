ea77a137322d26a60adfbd9b771d846a389192df
eons <eons@eons.dev>
2023-06-18 11:58:09 -0700

    init

---
+++ src/email_to_matrix.py
@@ -0,0 +1,20 @@
+from ebbs import Buildre
+import eons
+import logging
+
+class email_to_matrix(Builder):
+	def __init__(this, name = "Email to Matrix"):
+		super().__init__(name)
+
+	@eons.method(impl="External")
+	def get_email(this):
+		pass
+
+	@eons.method(impl="External")
+	def send_message_to_matrix(this):
+		pass
+
+	def Build(this):
+		emails = this.get_email()
+		for email in emails:
+			this.send_message_to_matrix(message=email['summary'])
