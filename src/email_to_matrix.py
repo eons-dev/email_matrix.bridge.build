0a2a8f2d8a2390c1e4d9831d612350e9307f7075
eons <eons@eons.dev>
2023-10-22 18:08:43 -0700

    use kind

--- src/email_to_matrix.py
+++ src/email_to_matrix.py
@@ -2,28 +2,21 @@ from ebbs import Builder
 import eons
 import logging
 
-class email_to_matrix(Builder):
-	def __init__(this, name = "Email to Matrix"):
-		super().__init__(name)
-
-		this.optionalKWArgs['message_length'] = 500
-
-	@eons.method(impl="External")
-	def get_email(this):
-		pass
-
-	@eons.method(impl="External")
-	def send_message_to_matrix(this):
-		pass
-
-	def Build(this):
-		emails = this.get_email()
-		for email in emails:
-			message = f"{email.subject}\n---\n"
-			if (email.summary):
-				message += email.summary
-			elif (email.body):
-				message += email.body[:this.message_length]
-			message = message.replace("\\n", "<br />")
-			message = message.replace("\n", "<br />")
-			this.send_message_to_matrix(message=message)
+@eons.kind(Builder)
+def email_to_matrix(
+	message_length = 500,
+	public = eons.public_methods(
+		'get_email',
+		'send_message_to_matrix'
+	)
+):
+	emails = get_email().result.data.returned
+	for email in emails:
+		message = f"{email.subject}\n---\n"
+		if (email.summary):
+			message += email.summary
+		elif (email.body):
+			message += email.body[:message_length]
+		message = message.replace("\\n", "<br />")
+		message = message.replace("\n", "<br />")
+		send_message_to_matrix(message=message)
